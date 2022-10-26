85bac5f5eedf24b0d774b86c295b4e8083a720b1
teknogeek <joel@teknogeek.io>
2022-10-26 11:33:14 -0700

    Start redoing how raffle odds work

--- bot.py
+++ bot.py
@@ -322,7 +322,6 @@ class RaffleCog(app_commands.Group, name="raffle"):
         await RaffleCog._end_raffle_impl(interaction, raffle_message_id, raffle_type, num_winners, [])
         DB().close_raffle(interaction.guild.id)
 
-    @staticmethod
     async def _end_raffle_impl(
         interaction: Interaction,
         raffle_message_id: int,
@@ -349,41 +348,35 @@ class RaffleCog(app_commands.Group, name="raffle"):
 
         entrants = set(u for u in entrant_list if u.id not in ineligible_winner_ids)
 
-        # Certain servers may only want you to be eligible for a raffle if you have
-        # given role(s). These are checked as ORs meaning if you have at least one
-        # of the configured roles you are eligible to win.
-        eligible_role_ids = DB().eligible_role_ids(guild_id)
-        if len(eligible_role_ids) > 0:
-            for entrant in entrants.copy():
-                if eligible_role_ids.intersection(RaffleCog._get_role_ids(entrant)) == set():
-                    entrants.remove(entrant)
-
         if len(entrants) == 0:
             await interaction.response.send_message("No one eligible entered the raffle so there is no winner.")
             return
 
-        if raffle_type == RaffleType.Normal:
-            winners = RaffleCog._choose_winners_weighted(guild_id, list(entrants), num_winners)
-        else:
-            winners = RaffleCog._choose_winners_unweighted(list(entrants), num_winners)
-
-        if raffle_type != RaffleType.Anyone:
-            DB().record_win(guild_id, raffle_message_id, *winners)
+        winners = RaffleCog.choose_winners(guild_id, list(entrants), num_winners)
+        DB().record_win(guild_id, raffle_message_id, *winners)
 
         if len(winners) == 1:
             await interaction.response.send_message(f"{winners[0].mention} has won the raffle!")
         else:
             await interaction.response.send_message(
-                f"Raffle winners are: {', '.join(map(lambda w: w.mention, winners))}!"
+                f"Raffle winners are: {', '.join(w.mention for w in winners)}!"
             )
 
-
-    @staticmethod
-    def _choose_winners_unweighted(
-        entrants: list[Member], num_winners: int
+    def choose_winners(
+        guild_id: int, entrants: list[Member], num_winners: int
     ) -> list[Member]:
+        """
+        Every raffle entry starts with 100 "tickets". Certain roles will get extra tickets.
+
+        Then we let numpy.random.choice work its magic.
+        """
         if len(entrants) < num_winners:
             raise Exception("There are not enough entrants for that many winners.")
+
+        # step 1. all entrants start with 100 tickets
+        entrant_tickets = {
+            e.id: 100 for e in entrants
+        }
 
         winners = []
         while num_winners > 0:
@@ -393,122 +386,6 @@ class RaffleCog(app_commands.Group, name="raffle"):
             num_winners -= 1
 
         return winners
-
-
-    @staticmethod
-    def _choose_winners_weighted(
-        guild_id: int, entrants: list[Member], num_winners: int
-    ) -> list[Member]:
-        """
-        Purpose of this algorithm is to choose winners in a way that actually lowers
-        their chances the more raffles they've won in the past.
-        Conceptually, can think of it as giving out more raffle "tickets" to those that have not won as often.
-
-        Each raffle win lowers your relative odds of winning by 25%.
-        So someone who has won once is 0.75x as likely to win as someone who has never won.
-        Someone who's won twice is 0.5625x (0.75^2) as likely as someone who has never won.
-        And so on.
-
-        Here's how it works.
-
-        We start by fetching the past wins of everyone in the guild.
-        Then, of the current raffle entrants, we start with the person who's won the most times.
-        Going from that win count -> 0 we figure out the ticket distribution factor for each bucket of win counts.
-        Then we figure out how many tickets they should get for each bucket based on that distribution factor.
-        That then gives us the relative probability array that gets fed into random.choice
-
-        Here's an example.
-
-        Say we have the following entrants:
-        8 people who've won 0 times
-        5 people who have won 1 time
-        2 people who have won 2 times
-        1 person who has won 4 times
-
-        Highest win count is 4 wins so we start there.
-        That bucket awards 1 ticket and then we calculate the fewer-win bucket tickets:
-        4 wins -> 1 ticket
-        3 wins -> 4/3 (~1.3) tickets
-        2 wins -> 16/9 (~1.8) tickets
-        1 win -> 64/27 (~2.4) tickets
-        0 wins -> 256/81 (~3.16) tickets
-
-        This way: 4 wins gets 0.75x as many tickets as 3 wins,
-        3 wins gets 0.75x as many tickets as 2 wins, and so on.
-
-        Total tickets given out is the sum of each bucket's tickets the number of entrants:
-        8 entrants * 256/81 tickets
-        + 5 * 64/27
-        + 2 * 16/9
-        + 0 * 4/3
-        + 1 * 1
-        = ~41.7 tickets
-
-        Now, the p-list values should all sum up to 1. So we treat those tickets as
-        portions of a "single ticket" and give out those portions.
-        We do that by taking the reciprocal, so 1/41.7 = 0.0239857862
-
-        0.0239857862 now is the chance of winning if you were given one "ticket".
-        Then we divvy out those tickets according to the number awarded per win bucket.
-
-        So then we end with:
-        8 people get 256/81 * 0.0239857862 = 0.07580692922 "tickets"
-        5 people get 64/27 * 0.0239857862 = 0.05685519692 tickets
-        2 people get 16/9 * 0.0239857862 = 0.04264139769 tickets
-        1 person gets 1 * 0.0239857862 = 0.0239857862 tickets
-
-        As a check, if we add all those up, it should equal 1.
-        0.07580692922 * 8 + 0.05685519692 * 5 + 0.04264139769 * 2 + 0.0239857862 = 0.9999999999
-
-        So then for our p-list, our resultant structure is:
-        [0.07580692922, 0.07580692922, 0.07580692922, ..., 0.04264139769, 0.04264139769, 0.0239857862]
-
-        And we sort the corresponding entrants list by their win counts ascending so the two lists line up.
-        [0-wins entrant, 0-wins entrant, 0-wins entrant, ..., 2-wins entrant, 2-wins entrant, 4-wins entrant]
-
-        Then we let numpy.random.choice work its magic.
-        """
-        if len(entrants) < num_winners:
-            raise Exception("There are not enough entrants for that many winners.")
-
-        # Just to add even more randomness
-        random.shuffle(entrants)
-        random.shuffle(entrants)
-        random.shuffle(entrants)
-
-        past_winner_win_counts = DB().win_counts(guild_id)
-        entrants = sorted(
-            entrants, key=lambda entrant: past_winner_win_counts.get(entrant.id, 0)
-        )
-
-        total_win_counts = {}
-        for entrant in entrants:
-            entrant_past_wins = past_winner_win_counts.get(entrant.id, 0)
-            if entrant_past_wins not in total_win_counts:
-                total_win_counts[entrant_past_wins] = 1
-            else:
-                total_win_counts[entrant_past_wins] += 1
-
-        tickets_per_win_bucket = {}
-        highest_entrant_wins = max(total_win_counts.keys())
-        for i in range(highest_entrant_wins, -1, -1):
-            tickets_per_win_bucket[i] = (4 / 3) ** (highest_entrant_wins - i)
-
-        total_tickets = 0
-        for win, tickets in tickets_per_win_bucket.items():
-            total_tickets += total_win_counts.get(win, 0) * tickets
-
-        value_of_one_ticket = 1 / total_tickets
-        for win, multiplier in tickets_per_win_bucket.copy().items():
-            tickets_per_win_bucket[win] = multiplier * value_of_one_ticket
-
-        p_list = []
-        for win, tickets in reversed(tickets_per_win_bucket.items()):
-            for i in range(0, total_win_counts.get(win, 0)):
-                p_list.append(tickets)
-
-        return numpy.random.choice(entrants, num_winners, replace=False, p=p_list)
-
 
     @staticmethod
     def _get_role_ids(member: Member) -> set[int]:
--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -9,38 +9,31 @@ class Raffle(Base):
     id = Column(Integer, primary_key=True)
     guild_id = Column(Integer, nullable=False)
     message_id = Column(Integer, nullable=False, unique=True)
+    entries = relationship("RaffleEntry", back_populates="raffle")
 
     def __repr__(self):
         return f"Raffle(id={self.id!r}, guild_id={self.guild_id!r}, message_id={self.message_id!r})"
 
-class Entry(Base):
+class RaffleEntry(Base):
     __tablename__ = "raffle_entries"
 
     id = Column(Integer, primary_key=True)
+    raffle_id = Column(Integer, ForeignKey("raffles.id"))
     guild_id = Column(Integer, nullable=False)
     message_id = Column(Integer, nullable=False)
     user_id = Column(Integer, nullable=False)
+    raffle = relationship("Raffle", back_populates="entries")
 
     def __repr__(self):
-        return f"Entry(id={self.id!r}, guild_id={self.guild_id!r}, message_id={self.message_id!r}, user_id={self.user_id!r})"
+        return f"RaffleEntry(id={self.id!r}, raffle_id={self.raffle_id!r}, guild_id={self.guild_id!r}, message_id={self.message_id!r}, user_id={self.user_id!r})"
 
-class Win(Base):
-    __tablename__ = "past_wins"
+class RoleModifier(Base):
+    __tablename__ = "role_modifiers"
 
     id = Column(Integer, primary_key=True)
     guild_id = Column(Integer, nullable=False)
-    message_id = Column(Integer, nullable=False)
-    user_id = Column(Integer, nullable=False)
+    role_id = Column(Integer, nullable=True, unique=True)
+    role_name = Column(String, nullable=True)
 
     def __repr__(self):
-        return f"Win(id={self.id!r}, guild_id={self.guild_id!r}, message_id={self.message_id!r}, user_id={self.user_id!r})"
-
-class EligibleRole(Base):
-    __tablename__ = "eligible_roles"
-
-    id = Column(Integer, primary_key=True)
-    guild_id = Column(Integer, nullable=False)
-    role_id = Column(Integer, nullable=False, unique=True)
-
-    def __repr__(self):
-        return f"EligibleRole(id={self.id!r}, guild_id={self.guild_id!r}, role_id={self.role_id!r})"
+        return f"RoleModifier(id={self.id!r}, guild_id={self.guild_id!r}, role_id={self.role_id!r})"

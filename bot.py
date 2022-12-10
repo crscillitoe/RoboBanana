6523a9eedee6d1e336614f2f5810113930c9bf2b
Joel Margolis <joel@teknogeek.io>
2022-12-09 16:46:30 -0800

    Proper role detection using raw msg info

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from discord import (
     app_commands,
     Client,
     Intents,
-    Member,
     Message,
 )
 from commands.mod_commands import ModCommands
@@ -48,22 +47,33 @@ class RaffleBot(Client):
         if message.author == self.user:
             return
 
+        # Server Subscription message
+        if message.channel.id == WELCOME_CHAT_ID and message.type.value == 25:
+            # fetch extra attached message info
+            raw_msg = await self.http.get_message(channel_id=message.channel.id, message_id=message.id)
+
+            role_sub_data = raw_msg.get("role_subscription_data")
+            if role_sub_data is None:
+                logging.error(f"Unable to get role subscription data for message: {message.id}")
+                return
+
+            # comes in like this -> 'tier_name': 'THE ONES WHO KNOW membership',
+            # trim " membership" and get the actual role
+            role_name = role_sub_data.get("tier_name", "").rstrip(" membership")
+            role = next(filter(lambda x: x.name.startswith(role_name), message.guild.roles))
+
+            # create the thank you message
+            if role_sub_data.get("is_renewal", False):
+                num_months = role_sub_data.get("total_months_subscribed", 1)
+                thankyou_message = f"Thank you {message.author.mention} for resubscribing to {role.name} for {num_months} months!"
+            else:
+                thankyou_message = f"Thank you {message.author.mention} for subscribing to {role.name}!"
+
+            return await self.get_channel(STREAM_CHAT_ID).send(thankyou_message)
+
         # Only look in the active stream channel
         if message.channel.id == STREAM_CHAT_ID:
             DB().accrue_channel_points(message.author.id, message.author.roles)
-
-    async def on_member_update(self, before: Member, after: Member):
-        new_roles = set(after.roles) - set(before.roles)
-        if len(new_roles) == 0:
-            return
-
-        for role in new_roles:
-            if role.id not in PREMIUM_IDS:
-                continue
-
-            return await self.get_channel(STREAM_CHAT_ID).send(
-                f"Thank you {after.mention} for joining {role.name}!"
-            )
 
 client = RaffleBot()
 tree = app_commands.CommandTree(client)

0e216fc862a5997424b0eae68a9c272ef26d9e22
Joel Margolis <joel@teknogeek.io>
2022-12-10 08:41:15 -0800

    Bugfix

--- controllers/sub_controller.py
+++ controllers/sub_controller.py
@@ -8,6 +8,11 @@ import requests
 STREAM_CHAT_ID = int(Config.CONFIG["Discord"]["StreamChannel"])
 AUTH_TOKEN = Config.CONFIG["Server"]["AuthToken"]
 PUBLISH_URL = "http://localhost:3000/publish-sub"
+PREMIUM_IDS = list(map(int, [
+    Config.CONFIG["Discord"]["Tier1RoleID"],
+    Config.CONFIG["Discord"]["Tier2RoleID"],
+    Config.CONFIG["Discord"]["Tier3RoleID"],
+]))
 
 LOG = logging.getLogger(__name__)
 
@@ -15,33 +20,37 @@ LOG = logging.getLogger(__name__)
 class SubController:
     @staticmethod
     async def subscribe(message: Message, client: Client):
-        premium_ids = map(
-            int,
-            [
-                Config.CONFIG["Discord"]["Tier1RoleID"],
-                Config.CONFIG["Discord"]["Tier2RoleID"],
-                Config.CONFIG["Discord"]["Tier3RoleID"],
-            ],
-        )
+        # fetch extra attached message info
+        raw_msg = await client.http.get_message(channel_id=message.channel.id, message_id=message.id)
 
-        role_name = None
-        for role_id in premium_ids:
-            role = discord.utils.get(message.author.roles, id=role_id)
-            if role is not None:
-                role_name = role.name
-                break
+        role_sub_data = raw_msg.get("role_subscription_data")
+        if role_sub_data is None:
+            return logging.error(f"Unable to get role subscription data for message: {message.id}")
 
-        if role_name is not None:
-            await client.get_channel(STREAM_CHAT_ID).send(
-                f"Thank you {message.author.mention} for joining {role_name}!"
-            )
-            Thread(
-                target=publish_update,
-                args=(
-                    message.author.name,
-                    role_name,
-                ),
-            ).start()
+        # comes in like this -> 'tier_name': 'THE ONES WHO KNOW membership',
+        # trim " membership" and get the actual role
+        membership_name = role_sub_data.get("tier_name", "").rstrip(" membership")
+        role = next(filter(lambda x: x.name.startswith(membership_name) and x.id in PREMIUM_IDS, message.guild.roles), None)
+        if role is None:
+            return logging.error(f"Unable to get role starting with: {membership_name}")
+
+        role_name = role.name
+
+        # create the thank you message
+        if role_sub_data.get("is_renewal", False):
+            num_months = role_sub_data.get("total_months_subscribed", 1)
+            thankyou_message = f"Thank you {message.author.mention} for resubscribing to {role_name} for {num_months} months!"
+        else:
+            thankyou_message = f"Thank you {message.author.mention} for subscribing to {role_name}!"
+
+        await client.get_channel(STREAM_CHAT_ID).send(thankyou_message)
+        Thread(
+            target=publish_update,
+            args=(
+                message.author.name,
+                role_name,
+            ),
+        ).start()
 
 
 def publish_update(name: str, role_name: str):

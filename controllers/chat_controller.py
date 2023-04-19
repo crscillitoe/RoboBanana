1d4bee86f379904ce0ef9a2ee5e0afaff479408a
Foyerr <43224440+Foyerr@users.noreply.github.com>
2023-04-19 17:14:57 -0300

    Add default_role to config.ini and modified chat_controller to include default_role
    Added defaultRoleID - this is the @everyone role fetched from discord.guild.default_role.id
    
    Then modified chat_modes to allow all roles in normal mode, remove t3 in t3_jail and only allow t3 in t3_only

--- controllers/chat_controller.py
+++ controllers/chat_controller.py
@@ -2,14 +2,17 @@ import discord
 from discord import Interaction
 from config import Config
 
+
 # Sub role ids
+
+DeafultRole = int(Config.CONFIG["Discord"]["defaultRoleID"])
 Tier1 = int(Config.CONFIG["Discord"]["Tier1RoleID"])
 Tier2 = int(Config.CONFIG["Discord"]["Tier2RoleID"])
 Tier3 = int(Config.CONFIG["Discord"]["Tier3RoleID"])
 GiftedTier1 = int(Config.CONFIG["Discord"]["GiftedTier1RoleID"])
 GiftedTier2 = int(Config.CONFIG["Discord"]["GiftedTier2RoleID"])
 GiftedTier3 = int(Config.CONFIG["Discord"]["GiftedTier3RoleID"])
-sub_roles = {Tier1, Tier2, Tier3, GiftedTier1, GiftedTier2, GiftedTier3}
+roles = {Tier1, Tier2, Tier3, GiftedTier1, GiftedTier2, GiftedTier3,DeafultRole}
 
 chat_modes = {
     "t3_only": {
@@ -17,11 +20,11 @@ chat_modes = {
         "slowmode": 0,
     },
     "normal": {
-        "allowed_roles": sub_roles,
+        "allowed_roles": roles,
         "slowmode": 15,
     },
     "t3_jail": {
-        "allowed_roles": sub_roles - {Tier3, GiftedTier3},
+        "allowed_roles": roles - {Tier3, GiftedTier3},
         "slowmode": 15,
     },
 }
@@ -32,7 +35,7 @@ class ChatController:
         if mode not in chat_modes:
             await interaction.response.send_message(f"Invalid chat mode: {mode}")
             return
-
+    
         chat_mode = chat_modes[mode]
         allowed_roles = chat_mode["allowed_roles"]
         slowmode = chat_mode["slowmode"]
@@ -44,6 +47,8 @@ class ChatController:
                 overwrites[role] = discord.PermissionOverwrite(send_messages=True)
             else:
                 overwrites[role] = discord.PermissionOverwrite(send_messages=False)
+            
+
 
         await interaction.channel.edit(overwrites=overwrites, slowmode_delay=slowmode)
         await interaction.response.send_message(f"Chat mode set to {mode}")

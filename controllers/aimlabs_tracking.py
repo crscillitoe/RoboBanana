4168a9a93c7066aea22d4a1faef20aa6fef9702a
braddotcoffee <braddotcoffee@users.noreply.github.com>
2023-06-29 02:38:00 +0000

    Apply Black formatting changes

--- controllers/aimlabs_tracking.py
+++ controllers/aimlabs_tracking.py
@@ -7,7 +7,10 @@ class AimlabsTrackingController:
     async def register_user(aimlabs_id: str, timezone: str, interaction: Interaction):
         if timezone not in pytz.all_timezones_set:
             return await interaction.response.send_message(
-                "Invalid timezone provided - please view valid timezones via `/aimlabs timezones`",
+                (
+                    "Invalid timezone provided - please view valid timezones via"
+                    " `/aimlabs timezones`"
+                ),
                 ephemeral=True,
             )
         DB().register_user(interaction.user.id, aimlabs_id, timezone)

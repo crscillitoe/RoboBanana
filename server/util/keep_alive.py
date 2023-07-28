97175d5b72cbbbee0c91585adc03d709fb6cb3c0
bwbonanno <bradfordbonanno@gmail.com>
2023-07-28 20:40:14 +0000

    Publish keepalive only to events channel

--- server/util/keep_alive.py
+++ server/util/keep_alive.py
@@ -1,16 +1,6 @@
 import asyncio
 import logging
-from server.util.constants import (
-    COOL_CHANNEL,
-    POLL_ANSWERS_CHANNEL,
-    POLLS_CHANNEL,
-    PREDICTIONS_CHANNEL,
-    SUBS_CHANNEL,
-    SUBS_COUNT_CHANNEL,
-    TAMAGACHI_CHANNEL,
-    TIMER_CHANNEL,
-    VOD_REVIEW_CHANNEL,
-)
+from server.util.constants import EVENTS_CHANNEL
 from quart import Quart
 from server.blueprints.sse import sse
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -20,15 +10,7 @@ logging.getLogger("apscheduler.executors.default").setLevel(logging.ERROR)
 
 async def keep_alive(app: Quart):
     async with app.app_context():
-        await sse.publish("\n\n", type="keepalive", channel=PREDICTIONS_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=SUBS_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=POLL_ANSWERS_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=POLLS_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=COOL_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=SUBS_COUNT_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=TIMER_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=VOD_REVIEW_CHANNEL)
-        await sse.publish("\n\n", type="keepalive", channel=TAMAGACHI_CHANNEL)
+        await sse.publish("\n\n", type="keepalive", channel=EVENTS_CHANNEL)
 
 
 def start_keepalive(app: Quart):

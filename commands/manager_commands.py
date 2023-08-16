6b8e71e8e0f22433ed85357732039449cd398359
bwbonanno <bradfordbonanno@gmail.com>
2023-08-16 16:09:35 +0000

    WIP manager commands temprole integration

--- commands/manager_commands.py
+++ commands/manager_commands.py
@@ -5,11 +5,19 @@ from config import Config
 import enum
 import logging
 
+from controllers.temprole_controller import TempRoleController
+
+APPROVED_TAG = int(Config.CONFIG["VODApproval"]["ApprovedTag"])
+REJECTED_TAG = int(Config.CONFIG["VODApproval"]["RejectedTag"])
+APPROVED_ROLE = int(Config.CONFIG["VODApproval"]["ApprovedRole"])
+REJECTED_ROLE = int(Config.CONFIG["VODApproval"]["RejectedRole"])
+
+LOG = logging.getLogger(__name__)
+
 
 class VODType(enum.Enum):
     approved = 1
     rejected = 2
-    complete = 3
 
 
 @app_commands.guild_only()
@@ -29,17 +37,41 @@ class ManagerCommands(app_commands.Group, name="manager"):
 
     @app_commands.command(name="flag_vod")
     @app_commands.checks.has_role("Community Manager")
-    @app_commands.describe(vod_type="VOD Type (Approved/Rejected/Complete)")
-    async def art(self, interaction: Interaction, vod_type: VODType) -> None:
+    @app_commands.describe(vod_type="VOD Type (Approved/Rejected)")
+    @app_commands.describe(duration="Duration to add to temprole")
+    async def flag_vod(
+        self, interaction: Interaction, vod_type: VODType, duration: str = "7d"
+    ) -> None:
         """Flag a VOD as the given type"""
 
-        tag_id = 0
         if vod_type == VODType.approved:
-            tag_id = 1055308435882774538
+            await ManagerCommands.process_vod(
+                APPROVED_TAG, APPROVED_ROLE, duration, interaction
+            )
         elif vod_type == VODType.rejected:
-            tag_id = 1055365088489508956
-        elif vod_type == VODType.complete:
-            tag_id = 1055504114978664498
+            await ManagerCommands.process_vod(
+                REJECTED_TAG, REJECTED_ROLE, duration, interaction
+            )
 
+    @staticmethod
+    async def process_vod(
+        tag_id: int, role_id: int, duration: str, interaction: Interaction
+    ):
+        role = interaction.guild.get_role(role_id)
+        if role is None:
+            return await interaction.response.send_message(
+                f"Could not find VOD role id={role_id}", ephemeral=True
+            )
+
+        owner = interaction.channel.owner
+        if owner is None:
+            return await interaction.response.send_message(
+                "Could not find author of this forum post", ephemeral=True
+            )
+
+        await TempRoleController.set_role(owner, role, duration, interaction)
+
+        await interaction.channel.remove_tags(*interaction.channel.applied_tags)
         await interaction.channel.add_tags(Object(id=tag_id))
-        await interaction.response.send_message("Applied tag.", ephemeral=True)
+        response_content = await interaction.original_response()
+        await response_content.reply("Applied tag and temprole.")

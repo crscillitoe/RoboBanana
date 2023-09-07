46a013317e5df0e3487d986779fcceddb47b7b38
braddotcoffee <braddotcoffee@users.noreply.github.com>
2023-09-07 18:27:46 +0000

    Apply Black formatting changes

--- util/server_utils.py
+++ util/server_utils.py
@@ -1,6 +1,8 @@
 from config import YAMLConfig as Config
+
 HOST = Config.CONFIG["Server"]["Host"]
 PORT = Config.CONFIG["Server"]["Port"]
+
 
 def get_base_url():
     return f"http://{HOST}:{PORT}"

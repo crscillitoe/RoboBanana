a47fafdd666a632fe65287ac809cf5f79c0fdc0b
crscillitoe <you@example.com>
2023-10-23 03:02:22 +0000

    twitch IRC listening functionality (sorry brad you gotta clean it up)

--- server/blueprints/chat.py
+++ server/blueprints/chat.py
@@ -1,3 +1,5 @@
+from quart import Blueprint, request
+from server.util.token_required import token_required
 from server.util.constants import (
     CHAT_MESSAGE_TEST_TYPE,
     CHAT_MESSAGE_STREAM_TYPE,
@@ -5,9 +7,21 @@ from server.util.constants import (
 )
 from .sse import sse
 import re
+import logging
 
 CUSTOM_EMOJI_PATTERN = re.compile(r"<:[a-zA-Z]+:[0-9]+>")
 
+chat_blueprint = Blueprint("chat", __name__)
+
+@chat_blueprint.route("/publish-chat", methods=["POST"])
+@token_required
+async def receive_chat():
+    try:
+        json = await request.get_json()
+        await sse.publish(json, type=CHAT_MESSAGE_STREAM_TYPE, channel=EVENTS_CHANNEL)
+        return ("OK", 200)
+    except (KeyError, ValueError):
+        return ("Bad Request", 400)
 
 async def publish_chat(chat_message):
     try:

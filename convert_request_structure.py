bd23e8648bf0b031d353f8f092174f4c2640fbf0
bwbonanno <bradfordbonanno@gmail.com>
2023-10-25 00:06:24 +0000

    Convert request structure script

---
+++ convert_request_structure.py
@@ -0,0 +1,61 @@
+import json
+
+TEXT_FIELDS = set(
+    [
+        "headerLeft",
+        "headerRight",
+        "sideBannerTextOne",
+        "sideBannerTextTwo",
+        "sideBannerTextThree",
+    ]
+)
+
+MEDIA_FIELDS = set(["backgroundVideo", "preRollVideo", "headerIcon", "sideBannerIcon"])
+
+
+def convert_to_text(value: str):
+    return {"type": "text", "value": value}
+
+
+def convert_to_media(source: str):
+    return {"type": "media", "source": source}
+
+
+def convert_timer(duration: int):
+    return {"duration": duration}
+
+
+def stay_same(value: any):
+    return value
+
+
+CONVERSION_MAPPING = {
+    "title": convert_to_media,
+    "timer": convert_timer,
+    "display": stay_same,
+    "scrollingText": stay_same,
+    "headerLeft": convert_to_text,
+    "headerRight": convert_to_text,
+    "sideBannerTextOne": convert_to_text,
+    "sideBannerTextTwo": convert_to_text,
+    "sideBannerTextThree": convert_to_text,
+    "backgroundVideo": convert_to_media,
+    "preRollVideo": convert_to_media,
+    "headerIcon": convert_to_media,
+    "sideBannerIcon": convert_to_media,
+}
+
+
+def main():
+    new_contents = {}
+    with open("./input.json", "r") as f:
+        original_contents: dict = json.loads(f.read())
+        for key, value in original_contents.items():
+            new_contents[key] = CONVERSION_MAPPING[key](value)
+
+    with open("./output.json", "w") as f:
+        f.write(json.dumps(new_contents, indent=2))
+
+
+if __name__ == "__main__":
+    main()

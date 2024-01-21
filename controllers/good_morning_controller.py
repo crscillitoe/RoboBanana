ed8a4782cc803eb58d8a8aff587008980d0142a3
Dark <7890309+DarkView@users.noreply.github.com>
2024-01-21 01:47:28 +0100

    Automatically distribute the GM role every sunday at 11pm
    This implementation grants the GM role as a temprole until 11:30pm on the upcoming sunday, making it so those that should keep their role never have to lose it and get a seamless transition.

--- controllers/good_morning_controller.py
+++ controllers/good_morning_controller.py
@@ -1,4 +1,4 @@
-from discord import Interaction, Thread
+from discord import Client, Interaction, Thread
 from db import DB
 from config import YAMLConfig as Config
 from datetime import datetime, timedelta
@@ -6,23 +6,33 @@ from time import mktime as epochtime
 from pytz import timezone
 import asyncio
 from controllers.temprole_controller import TempRoleController
+import logging
+from discord.ext import tasks
 
+LOG = logging.getLogger(__name__)
 
 STREAM_CHAT_ID = Config.CONFIG["Discord"]["Channels"]["Stream"]
 REWARD_ROLE_ID = Config.CONFIG["Discord"]["GoodMorning"]["RewardRole"]
 REWARD_REDEMPTION_CHANNEL_ID = Config.CONFIG["Discord"]["GoodMorning"][
     "RedemptionChannel"
 ]
+REWARD_PROGRESS_CHANNEL = Config.CONFIG["Discord"]["GoodMorning"]["ProgressChannel"]
 GOOD_MORNING_EXPLANATION = "What's this message? <#1064317660084584619>"
 
 PACIFIC_TZ = timezone("US/Pacific")
 UTC_TZ = timezone("UTC")
 START_TIME = PACIFIC_TZ.localize(datetime.utcnow().replace(hour=9, minute=0)).time()
 END_TIME = PACIFIC_TZ.localize(datetime.utcnow().replace(hour=14, minute=0)).time()
-GM_TEMPROLE_TIME = PACIFIC_TZ.localize(datetime.utcnow().replace(hour=23, minute=0)).time()
+# The time on the upcoming sunday the temporary GM role will expire. Setting this AFTER the automatic distribution time ensures a seamless role transition.
+GM_TEMPROLE_TARGET_TIME = PACIFIC_TZ.localize(datetime.utcnow().replace(hour=23, minute=30, second=0, microsecond=0)).time()
+# The time each day we run the `auto_reward_users` method. The method itself checks if it's sunday as that seems to be the easiest way to only do this on sundays.
+GM_TEMPROLE_AUTO_TIME = PACIFIC_TZ.localize(datetime.utcnow().replace(hour=23, minute=00, second=0, microsecond=0)).time()
 
 
 class GoodMorningController:
+    def __init__(self, client: Client):
+        self.client = client
+        
     async def get_morning_points(interaction: Interaction):
         points = DB().get_morning_points(interaction.user.id)
         await interaction.response.send_message(
@@ -86,6 +96,49 @@ class GoodMorningController:
             name="Good Morning Reward Progress"
         )
 
+        await GoodMorningController.tempreward_upcoming_sunday(thread, rewarded_user_ids, reward_role)
+
+        reward_message = (
+            f"Congrats {reward_role.mention}!"
+            f" Head over to <#{REWARD_REDEMPTION_CHANNEL_ID}> to redeem your reward!"
+        )
+        await interaction.followup.send(reward_message)
+
+    @tasks.loop(time=GM_TEMPROLE_AUTO_TIME)
+    async def auto_reward_users(self):
+        current_weekday = datetime.now(tz=PACIFIC_TZ).weekday()
+        if current_weekday != 6:
+            # fail silently as it is not sunday
+            return
+        
+        LOG.info("[AUTO GM TASK] Running automatic GM reward distribution...")
+        
+        rewarded_user_ids = DB().get_morning_reward_winners()
+        if len(rewarded_user_ids) == 0:
+            LOG.info("[AUTO GM TASK] No users to reward")
+            return
+
+        guild = self.client.get_guild(Config.CONFIG["Discord"]["GuildID"])
+        reward_role = guild.get_role(REWARD_ROLE_ID)
+        progres_channel = guild.get_channel(REWARD_PROGRESS_CHANNEL)
+
+        original_message = await progres_channel.send("Good Morning Reward Progress")
+        thread: Thread = await original_message.create_thread(
+            name="Good Morning Reward Progress",
+        )
+
+        await self.tempreward_upcoming_sunday(thread, rewarded_user_ids, reward_role)
+
+        DB().reset_all_morning_points()
+
+        reward_message = (
+            f"Congrats {reward_role.mention}!"
+            f" Head over to <#{REWARD_REDEMPTION_CHANNEL_ID}> to redeem your reward!"
+        )
+        await progres_channel.send(reward_message)
+
+    @staticmethod
+    async def tempreward_upcoming_sunday(thread, rewarded_user_ids, reward_role):
         reward_count = len(rewarded_user_ids)
 
         await thread.send(f"Distributing good morning rewards to {reward_count} users.")
@@ -97,7 +150,7 @@ class GoodMorningController:
         if day_delta == 0: # If it is currently sunday, manually add 7 days since we want the upcoming sunday, not the current one
             day_delta = 7
         upcoming_sunday = current_time + timedelta(days = day_delta)
-        upcoming_sunday = upcoming_sunday.replace(hour=GM_TEMPROLE_TIME.hour, minute=GM_TEMPROLE_TIME.minute)
+        upcoming_sunday = upcoming_sunday.replace(hour=GM_TEMPROLE_TARGET_TIME.hour, minute=GM_TEMPROLE_TARGET_TIME.minute)
         temprole_duration = int((upcoming_sunday - current_time) / timedelta(minutes=1)) # Convert to how many minutes are in this delta, then to int to drop any decimals eventhough there shouldn't be any
 
         # Assign roles
@@ -111,12 +164,6 @@ class GoodMorningController:
 
             # Rate limit
             await asyncio.sleep(1)
-
-        reward_message = (
-            f"Congrats {reward_role.mention}!"
-            f" Head over to <#{REWARD_REDEMPTION_CHANNEL_ID}> to redeem your reward!"
-        )
-        await interaction.followup.send(reward_message)
 
     async def reset_all_morning_points(interaction: Interaction):
         DB().reset_all_morning_points()

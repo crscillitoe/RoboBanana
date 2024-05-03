731d5c36912fa3b18d0253aee5fb217856485de0
Leshy <69055042+lorinvzyl@users.noreply.github.com>
2024-05-03 14:49:16 +0000

    Add defer function and modify reply function in discord_utils (#154)
    * Add defer function, updated reply function
    
    Reply function updated to support the use of the added defer function
    
    * Update CHANGELOG.md
    
    * Apply Black formatting changes
    
    * Fix extra static method and changed variable name
    
    ---------
    
    Co-authored-by: lorinvzyl <lorinvzyl@users.noreply.github.com>

--- util/discord_utils.py
+++ util/discord_utils.py
@@ -3,14 +3,25 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+DEFERRED_INTERACTION_IDS = []
+
 
 class DiscordUtils:
     @staticmethod
     async def reply(interaction: Interaction, *args, **kwargs):
         """Reply to an interaction regardless of if its been previously responded to"""
+        if interaction.id in DEFERRED_INTERACTION_IDS:
+            DEFERRED_INTERACTION_IDS.remove(interaction.id)
+            return await interaction.followup.send(*args, **kwargs)
         if interaction.response.is_done():
             return await interaction.followup.send(*args, **kwargs)
         return await interaction.response.send_message(*args, **kwargs)
+
+    @staticmethod
+    async def defer(interaction: Interaction, *args, **kwargs):
+        """Defer interaction response and hold interaction ID"""
+        DEFERRED_INTERACTION_IDS.append(interaction.id)
+        return await interaction.response.defer(*args, **kwargs)
 
     @staticmethod
     async def audit(
